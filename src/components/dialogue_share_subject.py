eafbf064ab2913707e3561d4e97a4c7a245b261f
riyanshu_07 <riyanshukandwal07@gmail.com>
2026-07-18 15:59:23 +0530

    Update Streamlit app domain

--- src/components/dialogue_share_subject.py
+++ src/components/dialogue_share_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog("Share class Link")
 def share_subject(subject_name,subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-main-riyanshu.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to join")
